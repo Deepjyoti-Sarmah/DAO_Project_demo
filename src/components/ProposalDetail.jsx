d791cacd0298b487f09ba1b2da0fc29cf3e69370
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-02-22 22:22:06 +0530

    added componets like home , proposal, index

---
+++ src/components/ProposalDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProposalDetail = () => {
+  return (
+    <div>ProposalDetail</div>
+  )
+}
+
+export default ProposalDetail
---
+++ src/views/Proposal.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProposalDetail from '../components/ProposalDetail'
+import Voters from '../components/Voters'
+
+const Proposal = () => {
+  return (
+    <>
+        <ProposalDetail/>
+        <Voters/>
+    </>
+  )
+}
+
+export default Proposal
